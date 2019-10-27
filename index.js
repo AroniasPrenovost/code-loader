f45211f262bbb9465b51f913551da713bbfd463a
Aron Prenovost <Aroniascoder@gmail.com>
2019-10-27 12:01:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,101 +1,89 @@
-var codeBars = [
-"bar1", "bar2", "bar3", 
-"bar4", "bar5", "bar6", 
-"bar7", "bar8", "bar9", 
-"bar10", "bar11", "bar12", 
-"bar13", "bar14", "bar15", 
-"bar16", "bar17", "bar18", 
-"bar19", "bar20", "bar21", 
-"bar22", "bar23", "bar24", 
-"bar25", "bar26", "bar27", 
-"bar28", "bar29", "bar30", 
-"bar31", "bar32", "bar33", 
-"bar34", "bar35", "bar36", 
-"bar37", "bar38", "bar39", 
-"bar40", "bar41"
-];
+// disable scrolling 
+document.body.style.overflow = 'hidden';
 
-var barLengths = [
-5, 6, 6, 12, 
-5, 5, 6, 8, 
-5, 6, 10, 6, 
-10, 6, 5, 8, 
-4, 7, 4, 8, 
-5, 8, 9, 7, 
-8, 10, 10, 3, 
-12, 3, 4, 11, 
-7, 8, 6, 4, 
-8, 6, 6, 6, 
-6
-];
-
-var lines = [
-"line1", "line2", "line3", "line4",
-"line5", "line6", "line7", "line8",
-"line9", "line10", "line11", "line12",
-"line13", "line14", "line15", "line16",
-"line17", "line18", "line19", "line20",
-"line21", "line22", "line23", "line24",
-"line25", "done"
-];
+////////////////////
+//
+// setInterval helper 
+//
+////////////////////
 
 function setIntervalX(callback, delay, repetitions) {
-  var x = 0;
-  var intervalID = window.setInterval(function () {
-
-    callback();
-
-    if (++x === repetitions) {
-      window.clearInterval(intervalID);
-    }
-  }, delay);
+    var x = 0;
+    var intervalID = window.setInterval(function () {
+        callback();
+        if (++x === repetitions) {
+            window.clearInterval(intervalID);
+        }
+    }, delay);
 }
 
+////////////////////
+//
+// codeblock generating function 
+//
+////////////////////
+
+var outer = document.getElementsByClassName('outer-animation')[0];
+var animationContainer = document.getElementsByClassName('inner-animation')[0];
+
+function codeBlocks(int, count) {
+    var div = document.createElement('div');
+    div.className = 'line';
+    for (var i = 0; i < int; i++) {
+        var innerDiv = document.createElement('div');
+        innerDiv.className = 'bar';
+        if (i < 1) {
+            if (count % 1 === 0) {
+                innerDiv.className += ' margin-small';
+            }
+            if (count % 2 === 0) {
+                innerDiv.className += ' margin-medium';
+            }
+            if (count % 3 === 0) {
+                innerDiv.className += ' margin-large';
+            }
+        }
+        div.append(innerDiv);
+    }
+    animationContainer.append(div);
+}
+
+// # of blocks in each row of 'code'  
+var blockMap = [1, 2, 3, 4, 3, 2, 1, 1, 2, 3, 4, 3, 2, 1, 1, 2, 3, 4, 3, 2, 1, 1, 2, 3, 4, 3, 2, 1, 1, 2, 3, 4, 3, 2, 1, 1, 2, 3, 4, 3, 2, 1, 1, 2, 3, 4, 3, 2, 1, 1, 2, 3, 4, 3, 2, 1, 1, 2, 3, 4, 3, 2, 1];
+for (var i = 0; i < blockMap.length; i++) {
+    codeBlocks(blockMap[i], i);
+}
+
+// animate code bars into visibility 
+var barCount = 0
+var speed = 80;
+var bars = document.getElementsByClassName('bar');
+var prev = 100;
+var random;
 setIntervalX(function () {
-  function move() {
-    var element = document.getElementById(codeBars[0]);
-    var barWidth = barLengths[0];
-    var width = 1;
-    var id = setInterval(frame, 1);
-    function frame() {
-      if (width >= barWidth ) {
-        clearInterval(id);
-      } else {
-        width++;
-        element.style.width = width + '%';
-      }
-    }
-    codeBars.shift();
-    barLengths.shift();
-  }
-  move();
+    random = Math.floor(Math.random() * 26) + 16;
+    if (random > prev) { random = (random - 3); }
+    bars[barCount].style.width = (random + 'px');
+    prev = random;
+    barCount++;
+}, speed, bars.length);
 
-}, 60, 41); // runs every 60 milliseconds, 41 times
+////////////////////
+//
+// animation removal
+//
+////////////////////
 
-// removes div elements
-setTimeout(function() {     
-  setInterval(function() {
-    if (lines[0] === "done") {
-      return;
-    }
-    var element1 = document.getElementById(lines[0]);
-    element1.outerHTML = "";
-    delete element1;
-    lines.shift();
-}, 100); // runs every 50 milliseconds
-}, 720);  // delays start time
+// minimuze height of block container 
+setTimeout(function () {
+    document.getElementsByClassName('animated-block')[0].style.height = 0;
+}, 250);
 
-/* 
-var s = document.getElementById('code-loader').style;
-s.opacity = 1;
-(function fade(){
-(s.opacity-=.09)<0?s.display="none":setTimeout(fade,500)
-}
-)();
-// removes code-container 
-setTimeout(function() {
-var element = document.getElementById("code-container");
-element.outerHTML = "";
-delete element;
-}, 2850);
-*/
+setTimeout(function () {
+    outer.style.opacity = 0;
+}, 2500);
+
+setTimeout(function () {
+    outer.style.display = 'none';
+    document.body.style.overflow = 'visible';
+}, 4500);
